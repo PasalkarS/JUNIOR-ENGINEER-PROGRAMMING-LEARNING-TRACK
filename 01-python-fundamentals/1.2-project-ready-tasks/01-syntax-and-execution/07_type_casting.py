9dba6be80c6bdd4624bf7520102abe84572398db
SamikshaPasalkar <147804419+PasalkarS@users.noreply.github.com>
2026-09-23 13:41:38 +0530

    Refactor type casting

--- 01-python-fundamentals/1.2-project-ready-tasks/01-syntax-and-execution/07_type_casting.py
+++ 01-python-fundamentals/1.2-project-ready-tasks/01-syntax-and-execution/07_type_casting.py
@@ -1,12 +1,11 @@
 # 07 - Type Casting
-# Converts between incompatible data types safely.
 
-str_num = "100"
-converted_int = int(str_num)
-float_val = float(converted_int)
+num = "100"
+integer = int(num)
+float = float(converted_int)
 back_to_str = str(float_val)
 
-print("Original str:", repr(str_num), type(str_num))
-print("To int:", converted_int, type(converted_int))
-print("To float:", float_val, type(float_val))
+print("Original str:", repr(num), type(num))
+print("To int:", integer, type(integer))
+print("To float:", float, type(float))
 print("Back to str:", repr(back_to_str), type(back_to_str))
