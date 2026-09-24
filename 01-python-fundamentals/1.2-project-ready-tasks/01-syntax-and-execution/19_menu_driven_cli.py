068d3d0cc939023b2be01b7cf5dfd75b3a2a7141
SamikshaPasalkar <147804419+PasalkarS@users.noreply.github.com>
2026-09-24 14:18:03 +0530

    menu program

---
+++ 01-python-fundamentals/1.2-project-ready-tasks/01-syntax-and-execution/19_menu_driven_cli.py
@@ -0,0 +1,14 @@
+print("1. Say Hello")
+print("2. Show Age")
+print("3. Exit")
+
+choice = input("Enter your choice: ")
+
+if choice == "1":
+    print("Hello!")
+elif choice == "2":
+    print("You are 24 years old.")
+elif choice == "3":
+    print("Goodbye!")
+else:
+    print("Invalid choice")
